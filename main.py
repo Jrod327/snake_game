d60fdafbc2cd40862964a0013b55ec671af04333
Jrod327 <113052919+Jrod327@users.noreply.github.com>
2022-09-13 16:40:29 -0400

    building

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 from turtle import Turtle, Screen
 
 screen = Screen()
@@ -8,6 +9,14 @@ screen.title("Jarod's Snake Game")
 snake = []
 x_pos = [0, -20, -40]
 
+
+def move_snake():
+    for i in range(len(snake)):
+        screen.tracer(25)
+        snake[i].forward(2)
+
+
+
 for i in range(0, 3):
     snake_piece = Turtle()
     snake_piece.penup()
@@ -15,8 +24,20 @@ for i in range(0, 3):
     snake_piece.color("white")
     snake_piece.goto(x=x_pos[i], y=0)
     snake.append(snake_piece)
+print(snake)
 
+screen.listen()
+screen.onkeypress(fun=move_snake, key="w")
 
+#look up tracer method
+
+# TODO 1: Create snake body
+# TODO 2: Move the snake
+# TODO 3: Create snake food
+# TODO 4: Detect collision with food
+# TODO 5: Create a scoreboard
+# TODO 6: Detect collision with wall
+# TODO 7: Detect collision with self
 
 
 
